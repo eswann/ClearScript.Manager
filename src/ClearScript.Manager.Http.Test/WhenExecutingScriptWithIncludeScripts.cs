7775fcf010a31534e801db371c28a4758e680674
Eric Swann <eric@ericswann.org>
2014-12-08 08:17:17 -0600

    Including project for http

---
+++ src/ClearScript.Manager.Http.Test/WhenExecutingScriptWithIncludeScripts.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using ClearScript.Manager.Http.Loaders;
+using NUnit.Framework;
+using Should;
+
+namespace ClearScript.Manager.Http.Test
+{
+
+    [TestFixture]
+    public class WhenExecutingScriptWithIncludeScripts
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            HttpScriptLoader.Register();
+        }
+
+        [Test]
+        public async void Script_With_Http_Include_Is_Included()
+        {
+            var subject = new TestObject();
+            var manager = new RuntimeManager(new ManualManagerSettings());
+
+            await manager.ExecuteAsync("testscript", "subject.Count = 10; subject.TestString = x;",
+                new ExecutionOptions
+                {
+                    HostObjects = new List<HostObject> {new HostObject {Name = "subject", Target = subject}},
+                    Scripts = new List<IncludeScript> { new IncludeScript { Uri = "https://gist.githubusercontent.com/eswann/3215f3afff3a602c0f3a/raw/a9ace53fca80fdcdefba60c4f7bf803bf5239905/gistfile1.txt", ScriptId = "testScript2" } }
+                });
+
+            subject.Name.ShouldEqual("Name");
+            subject.Count.ShouldEqual(10);
+            subject.TestString.ShouldEqual("test string1");
+        }
+
+        public class TestObject
+        {
+            private string _name = "Name";
+
+            public string Name
+            {
+                get { return _name; }
+                set { _name = value; }
+            }
+
+            public string TestString { get; set; }
+
+            public int Count { get; set; }
+        } 
+    }
+}
---
+++ src/ClearScript.Manager.Http/Loaders/HttpScriptLoader.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using ClearScript.Manager.Loaders;
+
+namespace ClearScript.Manager.Http.Loaders
+{
+    /// <summary>
+    /// Loads scripts if they are file-system based scripts.
+    /// </summary>
+    public class HttpScriptLoader : IScriptLoader
+    {
+        public static void Register()
+        {
+            ScriptLoadManager.RegisterLoader(new HttpScriptLoader());
+        }
+
+        public bool ShouldUse(IncludeScript script)
+        {
+            if (!string.IsNullOrEmpty(script.Code))
+                return false;
+
+            if (Uri.IsWellFormedUriString(script.Uri, UriKind.RelativeOrAbsolute))
+            {
+                var uri = new Uri(script.Uri);
+                if (!uri.IsFile)
+                    return true;
+            }
+
+            return false;
+        }
+
+        public async Task LoadCodeAsync(IncludeScript script)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                script.Code = await httpClient.GetStringAsync(script.Uri);
+            }
+        }
+    }
+}
