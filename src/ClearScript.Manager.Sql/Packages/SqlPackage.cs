f577f8c4bc4bd36d5d496009c21a023833f52bd9
owindog <412710591@qq.com>
2017-12-28 22:38:35 +0800

    up

---
+++ src/ClearScript.Manager.Sql/Packages/SqlPackage.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AntData.ORM.Data;
+using AntData.ORM.DataProvider;
+using AntData.ORM.DataProvider.MySql;
+using AntData.ORM.DataProvider.SqlServer;
+using ClearScript.Manager.Loaders;
+using ClearScript.Manager.Sql.Helpers;
+using Microsoft.ClearScript;
+
+namespace ClearScript.Manager.Sql.Package
+{
+    public class SqlExecutor : RequiredPackage
+    {
+        public SqlExecutor()
+        {
+            PackageId = "sqlExecutor";
+            HostObjects.Add(new HostObject { Name = "sqlExecutor", Target = new DbExecutor() });
+        }
+
+    }
+
+    public class SqlPackage : RequiredPackage
+    {
+        public SqlPackage()
+        {
+            PackageId = "sql";
+            ScriptUri = "ClearScript.Manager.Sql.Scripts.sql.js";
+            HostObjects.Add(new HostObject { Name = "xHost", Target = new ExtendedHostFunctions() });
+        }
+
+
+    }
+
+    public class SqlServerDb : DbContext
+    {
+        private static readonly IDataProvider _provider = new SqlServerDataProvider(SqlServerVersion.v2008);
+        public SqlServerDb(string dbMappingName) : base(dbMappingName)
+        {
+        
+        }
+
+        protected override IDataProvider provider
+        {
+            get { return _provider; }
+        }
+    }
+
+    public class MySqlServerDb : DbContext
+    {
+        private static readonly IDataProvider _provider = new MySqlDataProvider();
+        public MySqlServerDb(string dbMappingName) : base(dbMappingName)
+        {
+        }
+
+        protected override IDataProvider provider
+        {
+            get { return _provider; }
+        }
+    }
+
+    public class Table
+    {
+        
+    }
+}
---
+++ src/ClearScript.Manager.Sql/Packages/SqlPackageHelpers.cs
@@ -0,0 +1,20 @@
+﻿using ClearScript.Manager.Loaders;
+using ClearScript.Manager.Sql.Package;
+
+namespace ClearScript.Manager.Http.Packages
+{
+    /// <summary>
+    /// Helpers for registering packages.
+    /// </summary>
+    public static class SqlPackageHelpers
+    {
+        /// <summary>
+        /// Registers packages needed for using the request include.
+        /// </summary>
+        public static void RegisterSqlPackages()
+        {
+            RequireManager.RegisterPackage(new SqlExecutor());
+            RequireManager.RegisterPackage(new SqlPackage());
+        }
+    }
+}
