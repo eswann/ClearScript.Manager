fd2e50e4be1dea9eebe012ff7fb8a309f5535558
owindog <412710591@qq.com>
2017-12-30 11:57:01 +0800

    up

---
+++ src/JavaScript.Manager.Tabris/Packages/TabrisPackage.cs
@@ -0,0 +1,31 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="TabrisPackage.cs" company="Company">
+// Copyright (C) Company. All Rights Reserved.
+// </copyright>
+// <author>nainaigu</author>
+// <summary></summary>
+//-----------------------------------------------------------------------
+
+using JavaScript.Manager.Loaders;
+
+namespace JavaScript.Manager.Tabris.Packages
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public class TabrisPackage : RequiredPackage
+    {
+        public TabrisPackage()
+        {
+            PackageId = "tabris";
+            ScriptUri = "JavaScript.Manager.Tabris.Scripts.tabris.js";
+        }
+    }
+}
---
+++ src/JavaScript.Manager.Tabris/Packages/TabrisPackageHelpers.cs
@@ -0,0 +1,40 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="TabrisPackageHelpers.cs" company="Company">
+// Copyright (C) Company. All Rights Reserved.
+// </copyright>
+// <author>nainaigu</author>
+// <summary></summary>
+//-----------------------------------------------------------------------
+
+using JavaScript.Manager.Http.Packages;
+using JavaScript.Manager.Loaders;
+using JavaScript.Manager.Sql.Packages;
+
+namespace JavaScript.Manager.Tabris.Packages
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+
+    /// <summary>
+    /// 打包所有的
+    /// </summary>
+    public class TabrisPackageHelpers
+    {
+        public static void RegisterTabrisPackages()
+        {
+            //tabris
+            RequireManager.RegisterPackage(new TabrisPackage());
+
+            //sql
+            SqlPackageHelpers.RegisterSqlPackages();
+
+            //http
+            HttpPackageHelpers.RegisterRequestPackages();
+
+        }
+    }
+}
