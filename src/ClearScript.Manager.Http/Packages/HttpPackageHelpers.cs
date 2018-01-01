149d7e2de9c673d5ac9a93bd4ca5bd8fcd03c4ce
owindog <412710591@qq.com>
2018-01-01 15:44:52 +0800

    up

---
+++ src/ClearScript.Manager.Http/Packages/HttpPackageHelpers.cs
@@ -0,0 +1,19 @@
+﻿using JavaScript.Manager.Loaders;
+
+namespace JavaScript.Manager.Http.Packages
+{
+    /// <summary>
+    /// Helpers for registering packages.
+    /// </summary>
+    public static class HttpPackageHelpers
+    {
+        /// <summary>
+        /// Registers packages needed for using the request include.
+        /// </summary>
+        public static void RegisterPackage(RequireManager RequireManager)
+        {
+            RequireManager.RegisterPackage(new HttpPackage());
+            RequireManager.RegisterPackage(new RequestPackage());
+        }
+    }
+}
